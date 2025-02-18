fee5ab5902449ff51236e14d2ed85a40168606a5
Eduard Larionov <vesh95.17@ya.ru>
2025-02-18 12:07:19 +0500

    !draft: impl http handlers

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.23.2
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/pat v1.0.2 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	github.com/telegram-mini-apps/init-data-golang v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,101 +1,39 @@
 package main
 
 import (
+	v1 "backend/http/api/v1"
+	"backend/internal/api"
 	"backend/internal/canvas"
 	"flag"
-	"github.com/gorilla/websocket"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/pat"
+	"github.com/rs/cors"
+	"log"
+	"log/slog"
 	"net/http"
 	"sync"
+	"time"
 )
 
 var connectionsMap = sync.Map{}
 
 var addr = flag.String("addr", "localhost:3001", "http service address")
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-} // use default options
-
 var State = canvas.NewSyncCanvas()
 
 func main() {
-	//mux := http.NewServeMux()
-	//flag.Parse()
-	//log.SetFlags(0)
-	//mux.HandleFunc("/updates", connectionHandler)
-	//mux.HandleFunc("/set", setPixel)
-	//mux.HandleFunc("/state", getState)
-	//handler := cors.Default().Handler(mux)
-	//
-	//log.Fatal(http.ListenAndServe(*addr, handler))
-}
+	flag.Parse()
+	log.SetFlags(0)
 
-//func getState(w http.ResponseWriter, _ *http.Request) {
-//	err := json.NewEncoder(w).Encode(State.GetFull())
-//	w.Header().Set("Content-Type", "application/json")
-//	if err != nil {
-//		w.WriteHeader(500)
-//		log.Println(err)
-//	}
-//
-//	return
-//}
-//
-//// setPixel
-//func setPixel(w http.ResponseWriter, r *http.Request) {
-//	pixelData := canvas.PixelDto{}
-//	err := json.NewDecoder(r.Body).Decode(&pixelData)
-//	if err != nil {
-//		slog.Error("Decode error: %s", err)
-//		w.WriteHeader(http.StatusBadRequest)
-//		return
-//	}
-//	_, err = State.SetPixel(pixelData)
-//	slog.Info("Pixel", pixelData)
-//	if err != nil {
-//		slog.Error("pixel", err)
-//		w.WriteHeader(http.StatusBadRequest)
-//		_, _ = fmt.Fprint(w, err)
-//		return
-//	}
-//	_, err = broadcastUpdate(pixelData)
-//	if err != nil {
-//		slog.Error("Not send update pixel")
-//	}
-//
-//	w.WriteHeader(http.StatusOK)
-//	slog.Info("Pixel send")
-//}
-//
-//func broadcastUpdate(pixel canvas.PixelDto) (bool, error) {
-//	var err error
-//	connectionsMap.Range(func(key, value any) bool {
-//		conn := key.(*websocket.Conn)
-//		_ = conn.WriteJSON(pixel)
-//
-//		return true
-//	})
-//
-//	return err != nil, err
-//}
-//
-//func connectionHandler(w http.ResponseWriter, r *http.Request) {
-//	slog.Info("Connected")
-//	c, err := upgrader.Upgrade(w, r, nil)
-//	if err != nil {
-//		fmt.Println("Upgrade connection error:", err)
-//		return
-//	}
-//	defer c.Close()
-//
-//	connectionsMap.Store(c, true)
-//
-//	for {
-//		_, _, err := c.ReadMessage()
-//		if err != nil {
-//			connectionsMap.Delete(c)
-//			slog.Info("Disconnect: ", err)
-//			break
-//		}
-//	}
-//}
+	v1PixelController := v1.NewPixelController(State, map[string]interface{}{}, &slog.Logger{})
+	r := pat.New()
+	r.Get("/state", v1PixelController.GetState)
+	r.Post("/set", v1PixelController.SetPixel)
+	r.Get("/updates", v1PixelController.WSUpgrader)
+
+	m := mux.NewRouter()
+	m.Handle("/api/v1", cors.Default().Handler(r))
+	tbHandler := api.NewTokenBucket(1000, 1000, 100*time.Millisecond).RateLimitMiddleware(m)
+
+	log.Fatal(http.ListenAndServe(*addr, tbHandler))
+}
